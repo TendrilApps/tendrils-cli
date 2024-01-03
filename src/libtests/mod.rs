70732dbf9bdabbeb39f5d8f3b646abccae1ea7fc
Cole Markle <cmarkle.1995@gmail.com>
2024-01-03 08:12:41 -0500

    Renames file

--- src/libtests/mod.rs
+++ src/libtests/mod.rs
@@ -3,7 +3,7 @@ pub mod sample_tendrils;
 
 pub mod get_tendrils_folder_tests;
 pub mod get_tendrils_tests;
-pub mod get_tendril_override_tests;
+pub mod get_tendril_overrides_tests;
 pub mod is_tendrils_folder_tests;
 pub mod parse_tendrils_tests;
 pub mod pull_tests;
