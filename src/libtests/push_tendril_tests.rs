4e2a2b324d454f54f66af8389c60061c5661dc8c
Cole Markle <cmarkle.1995@gmail.com>
2024-02-08 06:07:48 -0500

    Adds test setup to push_tendril_tests

--- src/libtests/push_tendril_tests.rs
+++ src/libtests/push_tendril_tests.rs
@@ -1,9 +1,7 @@
-use crate::{push_tendril, ResolvedTendril, TendrilMode};
-use crate::test_utils::get_disposable_dir;
+use crate::{push_tendril, TendrilMode};
+use crate::test_utils::Setup;
 use crate::errors::TendrilActionError;
 use rstest::rstest;
-use std::fs::{create_dir_all, read_to_string, write};
-use tempdir::TempDir;
 
 #[rstest]
 fn given_link_mode_tendril_returns_mode_mismatch_error(
@@ -13,28 +11,14 @@ fn given_link_mode_tendril_returns_mode_mismatch_error(
     #[values(true, false)]
     force: bool,
 ) {
-    let temp_parent_dir = TempDir::new_in(
-        get_disposable_dir(),
-        "ParentDir"
-    ).unwrap();
-    let td_dir = temp_parent_dir.path().join("TendrilsDir");
-    let source = td_dir.join("SomeApp").join("misc.txt");
-    let dest = temp_parent_dir.path().join("misc.txt");
-    create_dir_all(source.parent().unwrap()).unwrap();
-    write(&source, "Source file contents").unwrap();
-    write(&dest, "Dest file contents").unwrap();
-    let given_tendril = ResolvedTendril::new(
-        "SomeApp".to_string(),
-        "misc.txt".to_string(),
-        temp_parent_dir.path().to_path_buf(),
-        TendrilMode::Link,
-    ).unwrap();
+    let setup = Setup::new();
+    setup.make_ctrl_file();
 
-    let actual = push_tendril(&td_dir, &given_tendril, dry_run, force);
+    let mut tendril = setup.resolved_file_tendril();
+    tendril.mode = TendrilMode::Link;
 
-    let source_file_contents = read_to_string(&source).unwrap();
-    let dest_file_contents = read_to_string(&dest).unwrap();
+    let actual = push_tendril(&setup.td_dir, &tendril, dry_run, force);
+
     assert!(matches!(actual, Err(TendrilActionError::ModeMismatch)));
-    assert_eq!(source_file_contents, "Source file contents");
-    assert_eq!(dest_file_contents, "Dest file contents");
+    assert!(!setup.local_file.exists());
 }
