8cca750c66836a3ad451f14ba42f925a35d45cbb
Cole Markle <cmarkle.1995@gmail.com>
2024-03-10 20:02:31 -0400

    Tests that tendril order in file is respected

--- tendrils/src/tests/get_tendrils_tests.rs
+++ tendrils/src/tests/get_tendrils_tests.rs
@@ -41,18 +41,24 @@ fn invalid_json_returns_parse_error() {
 }
 
 #[test]
-fn valid_json_returns_tendrils() {
+fn valid_json_returns_tendrils_in_same_order_as_file() {
     let temp_td_dir = TempDir::new_in(
         get_disposable_dir(),
         "TendrilsDir"
     ).unwrap();
-    let json = SampleTendrils::build_tendrils_json(
-        &[SampleTendrils::tendril_1_json()].to_vec(),
-    );
+    let json = SampleTendrils::build_tendrils_json(&vec![
+        SampleTendrils::tendril_1_json(),
+        SampleTendrils::tendril_4_json(),
+        SampleTendrils::tendril_2_json(),
+    ]);
     let tendrils_json = &temp_td_dir.path().join("tendrils.json");
     std::fs::write(&tendrils_json, &json).unwrap();
 
-    let expected = [SampleTendrils::tendril_1()].to_vec();
+    let expected = vec![
+        SampleTendrils::tendril_1(),
+        SampleTendrils::tendril_4(),
+        SampleTendrils::tendril_2(),
+    ];
 
     let actual: Vec<Tendril> = get_tendrils(&temp_td_dir.path()).unwrap();
 
