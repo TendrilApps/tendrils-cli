b6b8a7d8f6b645320fa2671aec2477b250c63b5e
Cole Markle <cmarkle.1995@gmail.com>
2024-07-01 21:54:05 -0400

    Updates metadata for td.exe on Windows

--- build.rs
+++ build.rs
@@ -2,7 +2,11 @@
 fn main() {
     let mut res = winresource::WindowsResource::new();
     res.set_icon("./assets/logo.ico");
-    res.compile().expect("Failed to run Windows Resource (rc.exe)");
+    res.set("FileDescription", "Tendrils");
+    res.set("ProductName", "Tendrils");
+    res.set("OriginalFilename", "td.exe");
+    res.set("CompanyName", "https://github.com/TendrilApps");
+    res.compile().expect("Failed to run rc.exe");
 }
 
 #[cfg(unix)]
