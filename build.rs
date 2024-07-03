819e05c8db9a5b9368cac57a89fad6dc3877d3e1
Cole Markle <cmarkle.1995@gmail.com>
2024-07-02 21:05:00 -0400

    Adds 'metadata' and 'logo' features

--- build.rs
+++ build.rs
@@ -1,12 +1,16 @@
 #[cfg(windows)]
 fn main() {
-    let mut res = winresource::WindowsResource::new();
-    res.set_icon("./assets/logo.ico");
-    res.set("FileDescription", "Tendrils");
-    res.set("ProductName", "Tendrils");
-    res.set("OriginalFilename", "td.exe");
-    res.set("CompanyName", "https://github.com/TendrilApps");
-    res.compile().expect("Failed to run rc.exe");
+    #[cfg(feature = "metadata")] {
+        let mut res = winresource::WindowsResource::new();
+
+        #[cfg(feature = "logo")]
+        res.set_icon("./assets/logo.ico");
+        res.set("FileDescription", "Tendrils");
+        res.set("ProductName", "Tendrils");
+        res.set("OriginalFilename", "td.exe");
+        res.set("CompanyName", "https://github.com/TendrilApps");
+        res.compile().expect("Failed to run rc.exe");
+    }
 }
 
 #[cfg(unix)]
